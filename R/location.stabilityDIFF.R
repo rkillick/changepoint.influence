1699efddfc6748ee9435bbeabb614924de67ce72
Rebecca Killick <r.killick@lancaster.ac.uk>
2021-06-24 14:09:43 +0100

    - Trying a different location stability plot suggested by a reviewer.

---
+++ R/location.stabilityDIFF.R
@@ -0,0 +1,100 @@
+location.stabilityDIFF=function(original.cpts, influence, data=NULL,include.data=FALSE,cpt.lwd=4,cpt.col=c("#009E73", "#E69F00", "#D55E00"),cpt.lty=c("dashed","dotdash","dotted"),ylab='',xlab='Index',...){
+  # histograms the changepoint locations identified
+  
+  # original.cpts     The cpts in the original data
+  # influence         The influenced cpts and parameters (output from influence.generate.** functions)
+  # data              Include if you want the data plotted
+  # include.data      True if you want to plot the data too
+  # hist.tcpt.delete  If true plots the histogram with the true changepoints deleted so you can see the moved changes more easily
+  # cpt.lwd           Line width for the changepoint lines
+  # cpt.col           Colour of the changepoint lines (length 3)
+  # cpt.lty           Line type for the changepoint lines (length 3)
+  # ylab              Label for the y-axis
+  # ...               Additional graphical parameters
+
+  col.cpts=list()
+  lty.cpts = list()
+  
+  n=nrow(influence[[1]]$class)
+  ncpts=length(original.cpts)
+
+  original.class=rep(1:(ncpts+1),times=diff(c(0,original.cpts,n)))
+  names=names(influence)
+  
+  for(i in 1:length(influence)){
+    method="Outlier"
+    max=n-2
+    if(names[i]=="delete"){
+      method="Deletion"
+      max=n-1
+      
+      # Dealing with the NAs temporarily (not returned to user) so we can plot nicely
+      index.na=which(is.na(influence[[i]]$class.del))[-1] # -1 as we will deal with the first instance separately
+      influence[[i]]$class.del[index.na]=influence[[i]]$class.del[index.na-n] # replace NA with previous index
+      influence[[i]]$class.del[1,1]=1 # replace the first NA with 1
+    }
+    
+    cpts=unlist(apply(influence[[i]]$class,1,FUN=function(x){which(diff(x)==1)}))
+    cpts=sort(cpts)
+    
+    if(names[i]=="delete"){
+      # create an index of cpts to delete as they are just a function of the deletion process
+      del.correct.index=apply(matrix(original.cpts,ncol=1),1,FUN=function(x){return(which(cpts==(x+1))[1])})
+      cpts=cpts[-del.correct.index]
+    }
+    else{
+      # create an index of cpts to delete as they are just a function of the modify process
+      del.outlier.index=apply(matrix(1:(n-1),ncol=1),1,FUN=function(x){return(which(cpts==x)[1:2])})
+      cpts=cpts[-del.outlier.index]
+    }
+    
+    fcpts=factor(cpts,levels=1:n) # factor so it includes all data points
+    tcpts=table(fcpts)
+    
+    col.cpts[[i]]=rep(cpt.col[1],length(original.cpts)) # "dark green"
+    lty.cpts[[i]]=rep(cpt.lty[1],length(original.cpts)) # "dashed" for "green"
+    for(j in 1:ncpts){
+      if(tcpts[which(names(tcpts)==as.character(original.cpts[j]))]!=max){
+        col.cpts[[i]][j]=cpt.col[2] # "orange2"
+        lty.cpts[[i]][j]=cpt.lty[2] # "dotdash" for "orange"
+      }
+    }
+    col.cpts[[i]][which(diff(original.cpts)==1)]=cpt.col[3] # red
+    col.cpts[[i]][which(diff(original.cpts)==1)+1]=cpt.col[3]
+    lty.cpts[[i]][which(diff(original.cpts)==1)]=cpt.lty[3] # "dotted" for "red
+    lty.cpts[[i]][which(diff(original.cpts)==1)+1]=cpt.lty[3]
+    names(col.cpts)[i]=names[i]
+    
+    tcpts[original.cpts]=tcpts[original.cpts]-max # difference from expected
+    
+    hist.col=rep(1,n)
+    hist.col[original.cpts]=col.cpts[[i]]
+
+    if(include.data==TRUE){
+      if(is.null(data)){
+        stop("data argument must be supplied if include.data=TRUE.")
+      }
+      op <- par(no.readonly = TRUE) # read current parameters
+      par(mfrow=c(2,1))
+      plot(data,type='l',ylab=ylab,xlab=xlab,main=paste('Location Stability: ',method,"method"),...) # plot the original time series
+      abline(v=original.cpts,col=col.cpts[[i]],lty=lty.cpts[[i]],lwd=cpt.lwd) # cpt.lty
+      
+      plot(tcpts,type='h',col=hist.col,xlab='Changepoint locations',ylab="Difference from expected",main='',...)
+      # start breaks at 0 as define the boundaries thus 1:n is n-1 breaks, not n
+      xaxp=par("xaxp")
+      axis(side=1,at=round(seq(from=xaxp[1],to=xaxp[2],length.out=xaxp[3]+1)),labels=round(seq(from=xaxp[1],to=xaxp[2],length.out=xaxp[3]+1)))
+      par(op) # reset previous parameters
+    }
+    else{ # same as above but title included on Histogram
+      plot(tcpts,col=hist.col,xaxt='n',ylim=c(min(tcpts),max(tcpts)),main=paste('Location Stability: ',method,"method"),ylab="Difference from expected",xlab='Changepoint locations',...)
+      xaxp=par("xaxp")
+      axis(side=1,at=round(seq(from=xaxp[1],to=xaxp[2],length.out=xaxp[3]+1)),labels=round(seq(from=xaxp[1],to=xaxp[2],length.out=xaxp[3]+1)))
+      # start breaks at 0 as define the boundaries thus 1:n is n-1 breaks, not n
+    }
+    # change colours to something meaningful to return to the user
+    col.cpts[[i]][which(col.cpts[[i]]==cpt.col[1])] = "stable"
+    col.cpts[[i]][which(col.cpts[[i]]==cpt.col[2])] = "unstable"
+    col.cpts[[i]][which(col.cpts[[i]]==cpt.col[3])] = "outlier"
+  }
+  return(col.cpts)
+}
