91756aa379b49db78c35e20879e6ba79d912d911
Rebecca Killick <r.killick@lancaster.ac.uk>
2023-04-20 18:36:09 +0100

    Revert "loo for k points"
    This reverts commit a880ef6237bc3e1ea54863ae9a7fab943f6af501.

--- R/loo.ind.cpt.R
+++ R/loo.ind.cpt.R
@@ -1,8 +1,7 @@
-loo.ind.cpt=function(del.point,k,data,...){
+loo.ind.cpt=function(del.point,data,...){
   # function to generate a single loo dataset and apply the PELT algorithm to the data
   
-  # del.point     The start index of the k points to delete
-  # k             number of points to modify simultaneously
+  # del.point     The index of the point to delete
   # data          The original data
   # ...           arguments to be passed to the changepoint method
   
@@ -11,8 +10,7 @@ loo.ind.cpt=function(del.point,k,data,...){
   else if(any(del.point>n)){stop('del.point is larger than the length of the data')}
   
   # generate the data
-  del.ind=del.point:(del.point+k-1)
-  data=data[-del.ind]
+  data=data[-del.point]
   
   # apply the changepoint approach to the reduced data
   del.ans=cpt.mean(data,...)
